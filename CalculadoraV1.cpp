2cdce6b88405c14450f23754dde8f84710d6b91f
JeffEnma10 <00402224.uca.edu.sv>
2026-01-14 14:46:07 -0600

    :memo: Subiendo Calculadora a la main

---
+++ CalculadoraV1.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int opcion;
+    double num1, num2, resultado;
+    
+do {
+        // Mostrar el menú
+        cout << "\n--- CALCULADORA ---\n";
+        cout << "1. Suma\n";
+        cout << "2. Resta\n";
+        cout << "3. Multiplicacion\n";
+        cout << "4. Division\n";
+        cout << "5. Salir\n";
+        cout << "Seleccione una opcion: ";
+        cin >> opcion;
+
+        // Pedir numeros si la opcion es correcta
+        if (opcion >= 1 && opcion <= 4) {
+            cout << "Ingrese el primer numero: ";
+            cin >> num1;
+            cout << "Ingrese el segundo numero: ";
+            cin >> num2;
+        }
+
+        switch (opcion) {
+            case 1:
+               resultado = num1 + num2;
+               cout << "Resultado de la suma: " << resultado << endl;
+               break;
+            case 2:
+                 resultado = num1 - num2;
+                 cout << "Resultado de la resta: " << resultado << endl;
+                 break;
+            case 3:
+                 resultado = num1 * num2;
+                 cout << "Resultado de la multiplicacion: " << resultado << endl;
+                 break;
+            case 4:
+            // Verificar division entre cero
+                if (num2 == 0) {
+                    cout << "Error no se puede dividir entre cero" << endl;
+    } else {
+        resultado = num1 / num2;
+        cout << "Resultado de la division: " << resultado << endl;
+    }
+    break;
+             case 5:
+                cout << "Saliendo del programa..." << endl;
+                break;
+
+            default:
+                cout << "Opcion no valida, intente de nuevo" << endl;
+        }
+        
+    } while (opcion != 5);
+ 
+
+
+
+return 0;
+}
