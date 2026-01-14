c44a05566ea150c30185688a9b91dab84b8bf31f
JeffEnma10 <00402224@uca.edu.sv>
2026-01-13 19:37:21 -0600

    Uniendo el menu
    Added a menu loop for calculator options and input handling.

--- CalculadoraV1.cpp
+++ CalculadoraV1.cpp
@@ -4,7 +4,37 @@ using namespace std;
 int main() {
     int opcion;
     double num1, num2, resultado;
+    
+do {
+        // Mostrar el menú
+        cout << "\n--- CALCULADORA ---\n";
+        cout << "1. Suma\n";
+        cout << "2. Resta\n";
+        cout << "3. Multiplicacion\n";
+        cout << "4. Division\n";
+        cout << "5. Salir\n";
+        cout << "Seleccione una opcion: ";
+        cin >> opcion;
 
+        // Pedir numeros si la opcion es correcta
+        if (opcion >= 1 && opcion <= 4) {
+            cout << "Ingrese el primer numero: ";
+            cin >> num1;
+            cout << "Ingrese el segundo numero: ";
+            cin >> num2;
+        }
+
+        switch (opcion) {
+             case 5:
+                cout << "Saliendo del programa..." << endl;
+                break;
+
+            default:
+                cout << "Opcion no valida, intente de nuevo" << endl;
+        }
+        
+    } while (opcion != 5);
+ 
 
 
 
